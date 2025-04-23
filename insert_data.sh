26ff629af59dbd68992f5484b28078e22cf6b63f
Justekeles <94258742+Justekeles@users.noreply.github.com>
2025-04-23 22:35:01 +0200

    Update insert_data.sh
    Added the wrong code

--- insert_data.sh
+++ insert_data.sh
@@ -1,42 +1,53 @@
 #! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
-echo -e "\nTotal number of goals in all games from winning teams:"
-echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
+# Remove all entries
+echo "$($PSQL "TRUNCATE games, teams")"
 
-echo -e "\nTotal number of goals in all games from both teams combined:"
-echo "$($PSQL "SELECT SUM(winner_goals + opponent_goals) FROM games")"
+# Insert from database
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != year ]]
+  then
+    # Add all winner teams to teams table IF NOT EXISTS
+    TEAM_NAME=$($PSQL "SELECT name FROM teams WHERE name='$WINNER'")
+    if [[ -z $TEAM_NAME ]]
+    then
+      INSERT_TEAM_NAME=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_TEAM_NAME = 'INSERT 0 1' ]]
+      then
+        echo "Inserted team $WINNER to table teams"
+      fi
+    fi
+    # Obtain winner's team_id to be able to populate games table
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
 
-echo -e "\nAverage number of goals in all games from the winning teams:"
-echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
+    # Add all loser teams to teams table IF NOT EXISTS
+    OPPONENT_NAME=$($PSQL "SELECT name FROM teams WHERE name='$OPPONENT'")
+    if [[ -z $OPPONENT_NAME ]]
+    then
+      INSERT_TEAM_NAME=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_TEAM_NAME == 'INSERT 0 1' ]]
+      then
+        echo "Inserted team $OPPONENT to table teams."
+      fi
+    fi
+    # Obtain loser's team_id to be able to populate games table
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
 
-echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
-echo "$($PSQL "SELECT ROUND(AVG(winner_goals),2) FROM games")"
-
-echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL "SELECT AVG(winner_goals + opponent_goals) FROM games")"
-
-echo -e "\nMost goals scored in a single game by one team:"
-echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
-
-echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals>2")"
-
-echo -e "\nWinner of the 2018 tournament team name:"
-echo "$($PSQL "SELECT name FROM games INNER JOIN teams ON games.winner_id = teams.team_id WHERE round='Final' AND year=2018")"
-
-echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "SELECT name FROM games INNER JOIN teams ON games.winner_id = teams.team_id OR games.opponent_id = teams.team_id WHERE round='Eighth-Final' AND year=2014 ORDER BY name ASC")"
-
-echo -e "\nList of unique winning team names in the whole data set:"
-echo "$($PSQL "SELECT DISTINCT name FROM games INNER JOIN teams ON games.winner_id = teams.team_id ORDER BY name ASC")"
-
-echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "SELECT year, name FROM games INNER JOIN teams ON games.winner_id = teams.team_id WHERE round='Final' ORDER BY year ASC")"
-
-echo -e "\nList of teams that start with 'Co':"
-echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%';")"
-
+    # Populate games table with data from games.csv and teams table
+    MATCH_INFO=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR, '$ROUND', '$TEAM_ID', '$OPPONENT_ID', $WINNER_GOALS, $OPPONENT_GOALS)")
+    if [[ $MATCH_INFO == 'INSERT 0 1' ]]
+    then
+      echo "Inserted the match $WINNER vs $OPPONENT to the games table."
+    fi
+  fi
+done
